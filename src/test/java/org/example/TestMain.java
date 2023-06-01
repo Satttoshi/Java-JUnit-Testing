66945d38d799da716441c05092bc2b1dec41814a
Josh <joshuastolle445@gmail.com>
2023-06-01 10:20:36 +0200

    initial repo

---
+++ src/test/java/org/example/TestMain.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class TestMain {
+    @Test
+    void test() {
+        int number1 = 2;
+        int number2 = 3;
+        int actual = Main.add(number1, number2);
+    }
+}
