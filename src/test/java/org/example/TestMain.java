fa7ccf21f92f3375e5772b51743763ce0a3cf1bb
Josh <joshuastolle445@gmail.com>
2023-06-01 10:21:39 +0200

    finish Test

--- src/test/java/org/example/TestMain.java
+++ src/test/java/org/example/TestMain.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class TestMain {
     @Test
@@ -8,5 +9,7 @@ public class TestMain {
         int number1 = 2;
         int number2 = 3;
         int actual = Main.add(number1, number2);
+        int expected = 5;
+        Assertions.assertEquals(expected, actual);
     }
 }
