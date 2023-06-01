ad222be7ec8f5e3a17a28ffbaaf5843ed9ca65ba
Josh <joshuastolle445@gmail.com>
2023-06-01 11:59:22 +0200

    finish Test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,32 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!" + add(1,2));
+        System.out.println("Add 2 numbers: " + add(1,2));
+        System.out.println("Is bigger than 100?: " + isBiggerThanHundred(101));
+        System.out.println("Fizzbuzz: " + fizzbuzz(7));
+        System.out.println("Smaller than zero?: " + smallerThanZero(2));
     }
-
     public static int add(int a, int b) {
         return a + b;
     }
+
+    public static boolean isBiggerThanHundred(int a) {
+        return a > 100;
+    }
+
+    public static String fizzbuzz(int number) {
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "fizzbuzz";
+        } else if (number % 3 == 0) {
+            return "fizz";
+        } else {
+            return "buzz";
+        }
+    }
+
+    public static boolean smallerThanZero(int number) {
+        return number < 0;
+    }
+
 }
--- src/test/java/org/example/TestMain.java
+++ src/test/java/org/example/TestMain.java
@@ -5,11 +5,59 @@ import org.junit.jupiter.api.Assertions;
 
 public class TestMain {
     @Test
-    void test() {
+    void testAddition() {
+
+        //Given
         int number1 = 2;
         int number2 = 3;
+
+        //When
         int actual = Main.add(number1, number2);
+
+        //Then
         int expected = 5;
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testIsBiggerThanHundred() {
+
+        //Given
+        int number = 100;
+
+        //When
+        boolean actual = Main.isBiggerThanHundred(number);
+
+        //Then
+        boolean expected = false;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testFizzbuzz() {
+
+        //Given
+        int number = 15;
+
+        //When
+        String actual = Main.fizzbuzz(number);
+
+        //Then
+        String expected = "fizzbuzz";
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void testSmallerThanZero() {
+
+            //Given
+            int number = -1;
+
+            //When
+            boolean actual = Main.smallerThanZero(number);
+
+            //Then
+            boolean expected = true;
+            Assertions.assertEquals(expected, actual);
     }
 }
